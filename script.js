a7f810fb01d7c635e33c02287b6fd5154e3d60ff
kiran-Jinka <kiranjinka@gmail.com>
2025-07-12 12:33:21 +0530

    script.js file added

---
+++ script.js
@@ -0,0 +1,5 @@
+    document.addEventListener('DOMContentLoaded', function() {
+    alert('Welcome to my simple HTML project!');
+    const heading = document.querySelector('h1');
+    heading.style.color = 'green';
+});
